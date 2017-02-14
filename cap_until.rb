064f284a3f291daf9d67af7c21ccd268ebc64ba4
James Sweeny <james.sweeny@puppetlabs.com>
2017-02-14 10:48:23 -0500

    Add command line options

--- cap_until.rb
+++ cap_until.rb
@@ -1,3 +1,5 @@
+require 'optparse'
+
 if Process.uid != 0
   puts "Must be run as root."
 end
@@ -6,17 +8,32 @@ def log msg=""
   puts "[#{Time.now}] #{msg}"
 end
 
-#device ||= 'eth0'
-device ||= 'en0'
-prefix ||= 'console_traffic'
-rotate ||= '300'
-filter ||= 'tcp port 4432 or tcp port 4430'
-watch_file ||= "test.log"
-watch_regex ||= / 504 /
+options = {}
+options[:interface] = 'eth0'
+options[:prefix] = 'console_traffic_'
+options[:rotate] = '300'
+options[:count] = 4
+options[:filter] = 'tcp port 4432 or tcp port 4430'
+options[:watch_file] = "test.log"
+options[:watch_regex] = / 504 /
+options[:exit_wait] = options
+
+OptionParser.new do |o|
+  o.on('-i INTERFACE_DEVICE') { |r| options[:interface] = r }
+  o.on('-l LOG_PREFIX') { |r| options[:prefix] = r }
+  o.on('-t ROTATION_FREQUENCY') { |r| options[:rotate] = r }
+  o.on('-c NUM_LOGS') { |r| options[:count] = r }
+  o.on('-f CAPTURE_FILTER') { |r| options[:filter] = r }
+  o.on('-r LOG_FILE') { |r| options[:watch_file] = r }
+  o.on('-x FILTER_REGEX') { |r| options[:watch_regex] = r }
+  o.on('-h') {puts o; exit 0}
+  o.parse!
+end
+
 scan_delay = 1
 
 @tcpdump = fork do
-  system('tcpdump', '-i', device, '-w' "#{prefix}%m%d%H%M.pcap", '-G', rotate, filter)
+  system('tcpdump', '-i', options[:interface], '-w', "#{options[:prefix]}%m%d%H%M.pcap", '-G', options[:rotate], options[:filter])
 end
 
 sleep 1 #wait for tcpdump to start
@@ -30,23 +47,24 @@ def quit_all exception=nil
   at_exit {}
   Process.kill "SIGKILL", @tcpdump + 1
   raise exception if exception != nil
-  exit 1
+  exit 0
 end
 
-def delayed_exit delay=5
+# Wait to kill tcpdump and exit until at least a full rotation has passed
+def delayed_exit delay=options[:rotate]
   sleep delay
   exit 0
 end
 
 begin
-  File.open(watch_file) do |file|
+  File.open(options[:watch_file]) do |file|
     file.seek(0,IO::SEEK_END)
     checking = true
     while checking do
       sleep scan_delay
       select([file])
       line = file.gets
-      if line =~ watch_regex
+      if line =~ options[:watch_regex]
         log "Found matching line: #{line}"
         checking = false
       end
