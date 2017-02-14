b7201d6fc1caa2802f5e6bf98704faee04a62f22
James Sweeny <james.sweeny@puppetlabs.com>
2017-02-14 10:57:45 -0500

    Set default exit delay

--- cap_until.rb
+++ cap_until.rb
@@ -2,6 +2,7 @@ require 'optparse'
 
 if Process.uid != 0
   puts "Must be run as root."
+  exit 1
 end
 
 def log msg=""
@@ -21,8 +22,8 @@ options[:exit_wait] = options
 OptionParser.new do |o|
   o.on('-i INTERFACE_DEVICE') { |r| options[:interface] = r }
   o.on('-l LOG_PREFIX') { |r| options[:prefix] = r }
-  o.on('-t ROTATION_FREQUENCY') { |r| options[:rotate] = r }
-  o.on('-c NUM_LOGS') { |r| options[:count] = r }
+  o.on('-t ROTATION_FREQUENCY', Integer) { |r| options[:rotate] = r }
+  o.on('-c NUM_LOGS', Integer) { |r| options[:count] = r }
   o.on('-f CAPTURE_FILTER') { |r| options[:filter] = r }
   o.on('-r LOG_FILE') { |r| options[:watch_file] = r }
   o.on('-x FILTER_REGEX') { |r| options[:watch_regex] = r }
@@ -33,7 +34,7 @@ end
 scan_delay = 1
 
 @tcpdump = fork do
-  system('tcpdump', '-i', options[:interface], '-w', "#{options[:prefix]}%m%d%H%M.pcap", '-G', options[:rotate], options[:filter])
+  system('tcpdump', '-i', options[:interface], '-w', "#{options[:prefix]}%m%d%H%M.pcap", '-G', "#{options[:rotate]}", options[:filter])
 end
 
 sleep 1 #wait for tcpdump to start
@@ -51,7 +52,7 @@ def quit_all exception=nil
 end
 
 # Wait to kill tcpdump and exit until at least a full rotation has passed
-def delayed_exit delay=options[:rotate]
+def delayed_exit delay=500
   sleep delay
   exit 0
 end
@@ -75,4 +76,4 @@ rescue Exception => e
   raise e
 end
 
-delayed_exit
+delayed_exit options[:rotate]
